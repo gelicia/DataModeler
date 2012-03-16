eebd46e2465a0f9fcf23623afcebecc0f5a609a7
Kyle Sletten <kbsletten@gmail.com>
2012-03-16 02:16:05 -0700

    (hopefully) fixed some concurrency issues

--- src/datamodeler/models/Connection.java
+++ src/datamodeler/models/Connection.java
@@ -12,55 +12,62 @@ import datamodeler.adapters.Adapter;
  * 
  */
 public class Connection {
-	private java.sql.Connection connection;
-	private Adapter adapter;
-	private Iterable<Database> databases;
+    private java.sql.Connection connection;
+    private Adapter adapter;
+    private Iterable<Database> databases;
 
-	private Connection(Adapter adapter) {
-		this.adapter = adapter;
+    private Connection(Adapter adapter) {
+	this.adapter = adapter;
+    }
+
+    public Connection(Adapter adapter, String host, int port, String username,
+	    String password) throws SQLException {
+	this(adapter);
+	this.connection = DriverManager.getConnection(this.adapter
+		.getConnectionString(host, port, username, password));
+    }
+
+    public synchronized Iterable<Database> getDatabases() throws SQLException {
+	synchronized (this) {
+	    if (this.databases == null) {
+		this.databases = this.adapter.getDatabases(this.connection
+			.createStatement());
+
+	    }
 	}
+	return this.databases;
+    }
 
-	public Connection(Adapter adapter, String host, int port, String username,
-			String password) throws SQLException {
-		this(adapter);
-		this.connection = DriverManager.getConnection(this.adapter
-				.getConnectionString(host, port, username, password));
+    public Iterable<Table> getTables(Database database) throws SQLException {
+	synchronized (database) {
+	    if (database.getTables() == null || database.getTables().isEmpty()) {
+		this.adapter.getTables(this.connection.createStatement(),
+			database);
+	    }
 	}
+	return database.getTables();
+    }
 
-	public synchronized Iterable<Database> getDatabases() throws SQLException {
-		if (this.databases == null) {
-			this.databases = this.adapter.getDatabases(this.connection
-					.createStatement());
+    public Iterable<Column> getColumns(Table table) throws SQLException {
+	synchronized (table) {
+	    if (table.getColumns() == null || table.getColumns().isEmpty()) {
+		this.adapter.getColumns(this.connection.createStatement(),
+			table);
+	    }
+	}
+	return table.getColumns();
+    }
+
+    public boolean loadMetaData() {
+	try {
+	    for (Database database : this.getDatabases()) {
+		for (Table table : this.getTables(database)) {
+		    this.getColumns(table);
 		}
-		return this.databases;
+	    }
+	    return true;
+	} catch (SQLException e) {
+	    return false;
 	}
-
-	public synchronized Iterable<Table> getTables(Database database)
-			throws SQLException {
-		if (database.getTables() == null || database.getTables().isEmpty()) {
-			this.adapter.getTables(this.connection.createStatement(), database);
-		}
-		return database.getTables();
-	}
-
-	public synchronized Iterable<Column> getColumns(Table table)
-			throws SQLException {
-		if (table.getColumns() == null || table.getColumns().isEmpty()) {
-			this.adapter.getColumns(this.connection.createStatement(), table);
-		}
-		return table.getColumns();
-	}
-
-	public boolean loadMetaData() {
-		try {
-			for (Database database : this.getDatabases()) {
-				for (Table table : this.getTables(database)) {
-					this.getColumns(table);
-				}
-			}
-			return true;
-		} catch (SQLException e) {
-			return false;
-		}
-	}
+    }
 }
--- src/datamodeler/threads/ThreadManager.java
+++ src/datamodeler/threads/ThreadManager.java
@@ -1,7 +1,7 @@
 package datamodeler.threads;
 
 public class ThreadManager {
-	public static void run(Runnable runnable) {
-		new Thread(runnable).start();
-	}
+    public static void run(Runnable runnable) {
+	new Thread(runnable).start();
+    }
 }
